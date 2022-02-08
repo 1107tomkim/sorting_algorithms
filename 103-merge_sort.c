814f6220a68b5e2aa15c490128924444865b6758
1107tomkim <cryptotaekim@gmail.com>
2022-02-08 05:29:47 +0000

    What i have so far

---
+++ 103-merge_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * merge_sort - actual merge sorting
+ * @array: whats being sorted
+ * @size: size of array
+ */
+void merge_sort(int *array, size_t size)
+{
+	int *temp;
+	free(temp);
+}
