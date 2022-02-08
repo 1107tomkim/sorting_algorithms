961f02a65012ed9b0b34b6320fb932f5d8046255
1107tomkim <cryptotaekim@gmail.com>
2022-02-08 05:56:55 +0000

    Big O sorting algorithm

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,6 +1,44 @@
 #include "sort.h"
 #include <stdlib.h>
 #include <stdio.h>
+/**
+ * _merge_sort - merge sort
+ * @array: whats being sorted
+ * @temp: temp array for holding
+ * @size: size of array
+ */
+void _merge_sort(int *array, int *temp, size_t size)
+{
+	size_t half = size / 2, i = 0, j = 0, k;
+
+	if (size < 2)
+		return;
+
+	_merge_sort(array, temp, half);
+	_merge_sort(array + half, temp + half, size - half);
+
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array(array, half);
+	printf("[right]: ");
+	print_array(array + half, size - half);
+	for (k = 0; k < size; k++)
+		if (j >= size - half || (i < half && array[i] < (array + half)[j]))
+		{
+			temp[k] = array[i];
+			i++;
+		}
+		else
+		{
+			temp[k] = (array + half)[j];
+			j++;
+		}
+	for (k = 0; k < size; k++)
+		array[k] = temp[k];
+	printf("[Done]: ");
+	print_array(array, size);
+}
+
 
 /**
  * merge_sort - actual merge sorting
@@ -10,5 +48,14 @@
 void merge_sort(int *array, size_t size)
 {
 	int *temp;
+
+	if (!array || size < 2)
+		return;
+
+	temp = malloc(sizeof(*temp) * size);
+	if (!temp)
+		return;
+
+	_merge_sort(array, temp, size);
 	free(temp);
 }
